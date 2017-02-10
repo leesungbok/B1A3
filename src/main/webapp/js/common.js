88a118f6dd61fe82f5772a48509731006f77cbd4
leesungbok <sungboklee93@gmail.com>
2017-02-10 10:37:49 +0900

    헤더 분리

---
+++ src/main/webapp/js/common.js
@@ -0,0 +1,37 @@
+$(function () {
+    // header.html을 가져와서 붙인다.
+    $.get('../header.html', function (result) {
+        $('#header').html(result);
+        
+        //경매등록 입력시 팝업창 표시
+        $('.form-control').popover();
+        
+        // 로그인시 로그인 페이지로 이동
+        $('#login-btn').click(function(event) {
+            location.href = "../auth/main.html";
+            event.preventDefault();
+        });
+        
+        // 회원가입시 회원가입 페이지로 이동
+        $('#join-btn').click(function(event) {
+            location.href = "../auth/joinEmail.html";
+            event.preventDefault();
+        });
+        
+        if (window.location == window.parent.location) {
+            $('#fullscreen').html('<span class="glyphicon glyphicon-resize-small"></span>');
+            $('#fullscreen').attr('href', 'http://bootsnipp.com/mouse0270/snippets/PbDb5');
+            $('#fullscreen').attr('title', 'Back To Bootsnipp');
+        }    
+        $('#fullscreen').on('click', function(event) {
+            event.preventDefault();
+            window.parent.location =  $('#fullscreen').attr('href');
+        });
+        $('#fullscreen').tooltip();
+        
+        $('.navbar-toggler').on('click', function(event) {
+            event.preventDefault();
+            $(this).closest('.navbar-minimal').toggleClass('open');
+        })
+    })
+})
