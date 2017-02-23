a4fa8887db72759ec48eab098b29fbdfc1978038
leesungbok <sungboklee93@gmail.com>
2017-02-23 19:30:37 +0900

    메인화면 작업중

--- src/main/java/bitcamp/java89/ems/service/ItemService.java
+++ src/main/java/bitcamp/java89/ems/service/ItemService.java
@@ -2,11 +2,14 @@ package bitcamp.java89.ems.service;
 
 import java.util.List;
 
+import bitcamp.java89.ems.domain.BidHistory;
 import bitcamp.java89.ems.domain.Item;
 
 public interface ItemService {
   List<Item> getList() throws Exception;
+  Item getNowBid() throws Exception;
   Item getDetail(int no) throws Exception;
+  List<BidHistory> getNowBidHistory(int itemNo) throws Exception;
   int add(Item item) throws Exception;
   int update(Item item) throws Exception;
   int delete(int itemNo) throws Exception;
