35171e37d879e5f6dca15043ea1dd473a4e4ec0b
Sana <Sana@DESKTOP-JFHL1GI>
2017-02-28 18:05:38 +0900

    나의경매

---
+++ src/main/java/bitcamp/java89/ems/domain/MyBid.java
@@ -0,0 +1,9 @@
+package bitcamp.java89.ems.domain;
+
+import java.io.Serializable;
+
+public class MyBid extends Item implements Serializable{
+  private static final long serialVersionUID = 1L;
+  
+  }
+
