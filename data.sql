a2516f54219689b965b7cef9238c1baca1a5b2bd
leesungbok <sungboklee93@gmail.com>
2017-03-03 19:03:22 +0900

    입찰하기 변경, DB 컬럼 이동

--- data.sql
+++ data.sql
@@ -55,13 +55,13 @@ insert into phot(itno,path) values(14,'성.gif');
 
 
 --관심 데이터
-insert into inter(mno,itno,type,rdt) values(1,10,1,'2017-01-01');
-insert into inter(mno,itno,type,rdt) values(2,11,1,'2017-01-01');
-insert into inter(mno,itno,type,rdt) values(3,13,1,'2017-01-01');
-insert into inter(mno,itno,type,rdt) values(2,14,1,'2017-01-01');
-insert into inter(mno,itno,type,rdt) values(4,10,1,'2017-01-01');
-insert into inter(mno,itno,type,rdt) values(4,12,1,'2017-01-01');
-insert into inter(mno,itno,type,rdt) values(2,13,1,'2017-01-01');
+insert into inter(mno,itno,type,rdt) values(1,1,1,'2017-01-01');
+insert into inter(mno,itno,type,rdt) values(2,2,1,'2017-01-01');
+insert into inter(mno,itno,type,rdt) values(3,3,1,'2017-01-01');
+insert into inter(mno,itno,type,rdt) values(2,4,1,'2017-01-01');
+insert into inter(mno,itno,type,rdt) values(4,5,1,'2017-01-01');
+insert into inter(mno,itno,type,rdt) values(4,6,1,'2017-01-01');
+insert into inter(mno,itno,type,rdt) values(2,7,1,'2017-01-01');
 
 
 
