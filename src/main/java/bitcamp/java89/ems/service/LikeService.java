75a5a8a0c1eec235663636d592b6e0cd31530fac
kimbora <kimbora@192.168.2.14>
2017-03-08 16:46:33 +0900

    .

--- src/main/java/bitcamp/java89/ems/service/LikeService.java
+++ src/main/java/bitcamp/java89/ems/service/LikeService.java
@@ -6,8 +6,11 @@ import bitcamp.java89.ems.domain.Like;
 
 public interface LikeService {
   List<Like> getLikeList(int memberNo) throws Exception;
+  List<Like> getrecentList(int memberNo) throws Exception;
   int add(Like like) throws Exception;
   int delete(int likeNo) throws Exception;
+  int update(Like like) throws Exception;
+  int recentDelete(int likeNo) throws Exception;
   String check(int memberNo, int itemNo) throws Exception;
   
   
